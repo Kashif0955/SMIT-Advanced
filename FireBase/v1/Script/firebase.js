88f5b7c663468048ca13047ee617db99325f0cd1
Malik Kashif <kashif2112a@aptechsite.net>
2024-07-26 23:55:25 +0500

    firebase first class

---
+++ FireBase/v1/Script/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+
+
+
+
+
---
+++ FireBase/v1/Script/signup.js
@@ -0,0 +1,39 @@
+import { auth,createUserWithEmailAndPassword } from "./firebase.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#signupForm");
+
+    form.addEventListener("submit", async (event) => {
+
+        try {
+            event.preventDefault();
+
+      
+            const username = document.getElementById('user').value;
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('pass').value;
+
+
+            const result = await createUserWithEmailAndPassword(auth, email, password);
+    
+            // Log data to console
+            // console.log("Username:", username);
+            // console.log("Email:", email);
+            // console.log("Password:", password);
+
+
+                 // Optionally, you can also log the entire form data object
+        // const formData = {
+        //     username,
+        //     email,
+        //     password
+        // };
+        // console.log("Form Data:", formData);
+        } catch (error) {
+            console.log("the error is:",error.message);
+        }
+       
+
+   
+    });
+});
